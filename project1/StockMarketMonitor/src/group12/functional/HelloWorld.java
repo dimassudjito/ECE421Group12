55955b6d5cae9e646be1e5e049d3da25ed8d0d7a
Dimas Sudjito <almuhtad@ualberta.ca>
2023-02-02 17:45:35 -0700

    init package for imperative and functional style

---
+++ project1/StockMarketMonitor/src/group12/functional/HelloWorld.java
@@ -0,0 +1,9 @@
+package group12.functional;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world functional");
+	}
+
+}
---
+++ project1/StockMarketMonitor/src/group12/imperative/HelloWorld.java
@@ -0,0 +1,8 @@
+package group12.imperative;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world imperative");
+	}
+}
