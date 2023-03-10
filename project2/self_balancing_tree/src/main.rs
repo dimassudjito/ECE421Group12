89bc7c8f319b2c773b000e3854ad472e53781081
Dimas Sudjito <almuhtad@ualberta.ca>
2023-03-10 11:47:23 -0700

    setup main.rs to test avl and gitignore

--- project2/self_balancing_tree/src/main.rs
+++ project2/self_balancing_tree/src/main.rs
@@ -1,3 +1,22 @@
+mod avl;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use avl::AVLTree;
+
+
+fn test_avl_tree() {
+    let mut root: AVLTree<&str> = AVLTree::Node { data: "5", left_child: Rc::new(RefCell::new(AVLTree::Empty)), right_child: Rc::new(RefCell::new(AVLTree::Empty)), height: 1 };
+    root.insert_node("3");
+    root.insert_node("2");
+    root.insert_node("4");
+    root.insert_node("7");
+    root.insert_node("6");
+    root.insert_node("9");
+    println!("Enum Root: {:#?}", root);
+}
+
 fn main() {
-    println!("Hello, world!");
+    test_avl_tree();
 }
