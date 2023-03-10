f3502801683e2498ad3fcb9b2d5d8cb5f1eceaef
Zony Yu <zony249@gmail.com>
2023-03-09 17:13:53 -0700

    first commit on rbtree

--- project2/self_balancing_tree/src/main.rs
+++ project2/self_balancing_tree/src/main.rs
@@ -1,3 +1,13 @@
+mod red_black;
+
+
+use red_black::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 fn main() {
     println!("Hello, world!");
+    let rbt = RedBlackTree::new(5);
+
+    rbt.print_hello_world();
 }
