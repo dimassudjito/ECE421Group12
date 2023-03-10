b1a0108825aab698dec741ec6292f94e06397c80
Zony Yu <zony249@gmail.com>
2023-03-09 17:12:12 -0700

    last commit on master, We will move to branch rbtree

--- project2/self_balancing_tree/src/red_black.rs
+++ project2/self_balancing_tree/src/red_black.rs
@@ -1,13 +1,13 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-enum  NodeColour {
+pub enum  NodeColour {
     Red, 
     Black,
 }
 
 
-enum RedBlackTree<T: Ord> {
+pub enum RedBlackTree<T: Ord> {
     Node {
         colour: NodeColour,
         data: T,
@@ -15,4 +15,20 @@ enum RedBlackTree<T: Ord> {
         right_child: Rc<RedBlackTree<T>>,
     },
     Empty
+}
+
+impl <T: std::cmp::Ord> RedBlackTree<T> {
+
+    pub fn new(data: T) -> Self {
+        RedBlackTree::<T>::Node {
+            colour: NodeColour::Black, 
+            data: data,
+            left_child: Rc::new(RedBlackTree::Empty),
+            right_child: Rc::new(RedBlackTree::Empty),
+        }
+    }
+    pub fn print_hello_world(&self) {
+        println!("Hello rb World!");
+    }
+
 }
