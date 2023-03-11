7ae03d8573cf084909434748b034c4574d863af5
jyw2 <jyw2@ualberta.ca>
2023-03-11 14:39:43 -0700

    Add in order traversal tested on trivial BST

--- project2/self_balancing_tree/src/avl.rs
+++ project2/self_balancing_tree/src/avl.rs
@@ -12,112 +12,104 @@ pub enum AVLTree<T: Ord> {
     Empty,
 }
 
-impl<T: Ord> AVLTree<T> {
-    pub fn insert_node(&mut self, new_data: T) {
-        // TODO: Dimas
-        match self {
-            AVLTree::Empty => {
-                *self = AVLTree::Node {
-                    data: new_data,
-                    left_child: Rc::new(RefCell::new(AVLTree::Empty)),
-                    right_child: Rc::new(RefCell::new(AVLTree::Empty)),
-                    height: 1,
-                };
-            }
-            AVLTree::Node { data, left_child, right_child, height } => {
-                if new_data < *data {
-                   let mut borrowed_node = left_child.borrow_mut();
-                    (*borrowed_node).insert_node(new_data);
-                } else if new_data > *data {
-                    let mut borrowed_node = right_child.borrow_mut();
-                    (*borrowed_node).insert_node(new_data);
-                } else {
-                    return;
-                }
-            }
-        }
-    }
+impl<T: Ord + std::fmt::Display> AVLTree<T> {
+    // pub fn insert_node(&mut self, new_data: T) {
+    //     // TODO: Dimas
+    //     match self {
+    //         AVLTree::Empty => {
+    //             *self = AVLTree::Node {
+    //                 data: new_data,
+    //                 left_child: Rc::new(RefCell::new(AVLTree::Empty)),
+    //                 right_child: Rc::new(RefCell::new(AVLTree::Empty)),
+    //                 height: 1,
+    //             };
+    //         }
+    //         AVLTree::Node { data, left_child, right_child, height } => {
+    //             if new_data < *data {
+    //                let mut borrowed_node = left_child.borrow_mut();
+    //                 (*borrowed_node).insert_node(new_data);
+    //             } else if new_data > *data {
+    //                 let mut borrowed_node = right_child.borrow_mut();
+    //                 (*borrowed_node).insert_node(new_data);
+    //             } else {
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
 
-    pub fn populateTestTree(&self){
-        self.insert_node(1);
-        self.insert_node(5);
-        self.insert_node(2);
-        self.insert_node(6);
-        self.insert_node(3);
-        self.insert_node(4);
-    }
+    // pub fn delete_node() {
+    //     // TODO: Josh
+    //     match self {
+    //         AVLTree::Empty => {
+    //             panic!("Node does not exist in tree")
+    //         }
+    //         AVLTree::Node { data, left_child, right_child } => {
+    //             if new_data < *data {
+    //                 left_child.delete_node(new_data);
+    //             } else if new_data > *data {
+    //                 right_child.delete_node(new_data);
+    //             } else {
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
 
-    pub fn delete_node() {
-        // TODO: Josh
-        match self {
-            Tree::Empty => {
-                panic!("Node does not exist in tree")
-            }
-            Tree::Node { data, left_child, right_child } => {
-                if new_data < *data {
-                    left_child.delete_node(new_data);
-                } else if new_data > *data {
-                    right_child.delete_node(new_data);
-                } else {
-                    return;
-                }
-            }
-        }
-    }
+    // pub fn rotation_left_left() {
+    //     // TODO: Dimas
+    // }
 
-    pub fn rotation_left_left() {
-        // TODO: Dimas
-    }
+    // pub fn rotation_left_right() {
+    //     // TODO: Dimas
+    // }
 
-    pub fn rotation_left_right() {
-        // TODO: Dimas
-    }
+    // pub fn rotation_right_left() {
+    //     // TODO: Josh
+    // }
 
-    pub fn rotation_right_left() {
-        // TODO: Josh
-    }
+    // pub fn rotation_right_right() {
+    //     // TODO: Josh
+    // }
 
-    pub fn rotation_right_right() {
-        // TODO: Josh
-    }
+    // pub fn leaf_number(&self) {
+    //     // TODO: Josh
 
-    pub fn leaf_number(&self) {
-        // TODO: Josh
+    //     fn dfs(node){
+    //         if ( node.left.borrow() == AVLTree::Empty &&
+    //         node.right.borrow() == AVLTree::Empty )
+    //             return 0
+    //         else:
+    //             return dfs(node.left.borrow()) + dfs(node.right.borrow());
+    //     }
+    // }
 
-        fn dfs(node){
-            if ( node.left.borrow() == AVLTree::Empty &&
-            node.right.borrow() == AVLTree::Empty )
-                return 0
-            else:
-                return dfs(node.left.borrow()) + dfs(node.right.borrow());
-        }
-    }
+    // pub fn tree_height() {
+    //     // TODO: Dimas
+    // }
 
-    pub fn tree_height() {
-        // TODO: Dimas
-    }
-
-    pub fn print() {
-        // TODO: Dimas
-    }
+    // pub fn print() {
+    //     // TODO: Dimas
+    // }
 
     pub fn print_inorder(&self) {
         // TODO: Josh
         match self {
-            Tree::Node {
+            AVLTree::Node {
                 data,
                 left_child,
                 right_child,
+                height
             } => {
-                left_child.borrow().print_tree();
+                (*left_child).borrow().print_inorder();
                 println!("{}", data);
-                right_child.borrow().print_tree();
+                (*right_child).borrow().print_inorder();
             }
-            Tree::Empty => return,
+            AVLTree::Empty => return,
         }
     }
 
-    pub fn is_tree_empty() {
-        // TODO: Dimas
-    }
+    // pub fn is_tree_empty() {
+    //     // TODO: Dimas
+    // }
 }
