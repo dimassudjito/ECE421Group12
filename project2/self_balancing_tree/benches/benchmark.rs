ec318f6631ab7f0b5f5adb73dd56e5a4747b4ad0
Zony Yu <zony249@gmail.com>
2023-03-18 12:23:04 -0600

    avl random and avl sequential

--- project2/self_balancing_tree/benches/benchmark.rs
+++ project2/self_balancing_tree/benches/benchmark.rs
@@ -10,6 +10,7 @@ use rand::thread_rng;
 use rand::Rng;
 use rand::prelude::SliceRandom;
 use std::time::Duration;
+use std::fmt::Display;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum  NodeColour {
@@ -633,6 +634,362 @@ impl <T: Ord + Copy + Debug> RedBlackTree<T> {
 }
 
 
+#[derive(Debug)]
+pub enum AVLTree<T: Ord> {
+    Node {
+        data: RefCell<Rc<T>>,
+        left_child: RefCell<Rc<AVLTree<T>>>,
+        right_child: RefCell<Rc<AVLTree<T>>>,
+        height: RefCell<i32>,
+    },
+    Empty,
+}
+
+impl<T: Ord + Display + Copy> AVLTree<T> {
+    pub fn insert_node(node_rc: &Rc<AVLTree<T>>, new_data: &T) -> Rc<AVLTree<T>> {
+        match &**node_rc {
+            AVLTree::Empty => {
+                let new_node: AVLTree<T> = AVLTree::Node {
+                    data: RefCell::new(Rc::new(*new_data)),
+                    left_child: RefCell::new(Rc::new(AVLTree::Empty)),
+                    right_child: RefCell::new(Rc::new(AVLTree::Empty)),
+                    height: RefCell::new(1),
+                };
+                Rc::new(new_node)
+            }
+            AVLTree::Node {
+                data,
+                left_child,
+                right_child,
+                height,
+            } => {
+                if *new_data < **(data.borrow()) {
+                    let new_node = AVLTree::insert_node(&*(left_child.borrow()), new_data);
+                    left_child.replace(new_node);
+                } else if *new_data > **(data.borrow()) {
+                    let new_node = AVLTree::insert_node(&*(right_child.borrow()), new_data);
+                    right_child.replace(new_node);
+                } else {
+                    return Rc::clone(node_rc);
+                }
+                
+                let return_node_rc = AVLTree::insert_node_balance(node_rc);
+                (*return_node_rc).update_heights();
+                return return_node_rc;
+            }
+        }
+    }
+
+    pub fn insert_node_balance(node_rc: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        match &**node_rc {
+            AVLTree::Empty => {
+                return Rc::clone(node_rc);
+            }
+            AVLTree::Node {
+                data,
+                left_child: left_child_ref,
+                right_child: right_child_ref,
+                ..
+            } => {
+                // balance
+                let left_node = &*Rc::clone(&*left_child_ref.borrow());
+                let right_node = &*Rc::clone(&*right_child_ref.borrow());
+
+                let left_height = (*left_child_ref.borrow()).get_height();
+                let right_height = (*right_child_ref.borrow()).get_height();
+                if (left_height - right_height).abs() > 1 {
+                    // this is an unbalanced node
+                    if left_height > right_height {
+                        // left-<?> case
+                        match left_node {
+                            AVLTree::Empty => {
+                                panic!("Given tree is not a proper AVL tree");
+                            }
+                            AVLTree::Node {
+                                left_child: y_left_child_ref,
+                                right_child: y_right_child_ref,
+                                ..
+                            } => {
+                                let y_left_height = (*y_left_child_ref.borrow()).get_height();
+                                let y_right_height = (*y_right_child_ref.borrow()).get_height();
+                                if y_left_height > y_right_height{
+                                    // left-left case
+                                    return AVLTree::rotation_left_left(node_rc);
+                                } else {
+                                    // left-right case
+                                    return AVLTree::rotation_left_right(node_rc);
+                                }
+                            }
+                        }
+                    } else {
+                        // right-<?> case
+                        match right_node {
+                            AVLTree::Empty => {
+                                panic!("Given tree is not a proper AVL tree");
+                            }
+                            AVLTree::Node {
+                                left_child: y_left_child_ref,
+                                right_child: y_right_child_ref,
+                                ..
+                            } => {
+                                let y_left_height = (*y_left_child_ref.borrow()).get_height();
+                                let y_right_height = (*y_right_child_ref.borrow()).get_height();
+                                if y_left_height > y_right_height {
+                                    // right-left case
+                                    return AVLTree::rotation_right_left(node_rc);
+                                } else {
+                                    // right-right case
+                                    return AVLTree::rotation_right_right(node_rc);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    // is a balanced node
+                    return Rc::clone(node_rc);
+                }
+            }
+        }
+    }
+
+    pub fn rotation_left_left(z: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        return AVLTree::rotate_right(z);
+    }
+
+    pub fn rotation_left_right(z: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        //   z
+        //  /
+        // y
+        match &(**z) {
+            AVLTree::Empty => {
+                panic!("Invalid AVLTree for left right rotation")
+            }
+            AVLTree::Node { left_child, .. } => {
+                let new_left = AVLTree::rotate_left(&(left_child.borrow()));
+                left_child.replace(new_left);
+                return AVLTree::rotate_right(z);
+            }
+        }
+    }
+
+    pub fn rotation_right_right(z: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        return AVLTree::rotate_left(z);
+    }
+
+    pub fn rotation_right_left(z: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        //   z
+        //    \
+        //     y
+        match &(**z) {
+            AVLTree::Empty => {
+                panic!("Invalid AVLTree for right left rotation")
+            }
+            AVLTree::Node { right_child, .. } => {
+                let new_right = AVLTree::rotate_right(&(right_child.borrow()));
+                right_child.replace(new_right);
+                return AVLTree::rotate_left(z);
+            }
+        }
+    }
+
+    pub fn rotate_right(z_rc: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        // println!("rotating right");
+
+        // EX:   z
+        //      /
+        //     y
+        //    / \
+        //   x   N
+        let z = &(**z_rc);
+        match z {
+            AVLTree::Empty => Rc::clone(z_rc),
+            AVLTree::Node {
+                left_child: z_left_child,
+                ..
+            } => {
+                let y_rc = Rc::clone(&z_left_child.borrow());
+                let y = &(*y_rc);
+                match y {
+                    AVLTree::Empty => Rc::clone(z_rc),
+                    AVLTree::Node {
+                        right_child: y_right_child,
+                        ..
+                    } => {
+                        //       y
+                        //      / \
+                        //     x   z      N
+                        let n = (y_right_child).replace(Rc::clone(z_rc));
+
+                        //       y
+                        //      / \
+                        //     x   z
+                        //        /
+                        //       n
+                        z_left_child.replace(n);
+
+                        y.update_heights();
+                        z.update_heights();
+
+                        y_rc
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn rotate_left(z_rc: &Rc<AVLTree<T>>) -> Rc<AVLTree<T>> {
+        // println!("rotating left");
+
+        // EX:   z
+        //        \
+        //         y
+        //        / \
+        //       n   x
+        let z = &(**z_rc);
+        match z {
+            AVLTree::Empty => Rc::clone(z_rc),
+            AVLTree::Node {
+                right_child: z_right_child,
+                ..
+            } => {
+                let y_rc = Rc::clone(&z_right_child.borrow());
+                let y = &(*y_rc);
+                match y {
+                    AVLTree::Empty => Rc::clone(z_rc),
+                    AVLTree::Node {
+                        left_child: y_left_child,
+                        ..
+                    } => {
+                        //       y
+                        //      / \
+                        //     z   x
+                        let n = (y_left_child).replace(Rc::clone(z_rc));
+
+                        //       y
+                        //      / \
+                        //     z   x
+                        //      \
+                        //        n
+                        z_right_child.replace(n);
+
+                        y.update_heights();
+                        z.update_heights();
+
+                        y_rc
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn update_heights(&self) {
+        // updates the heights of an node based on it's direct children's heights.
+        // IT IS NOT recursive. If the children's heights are incorrect, the height of this node will be as well.
+        // TODO: leaf node should be 1 not 0
+        match self {
+            AVLTree::Empty => {}
+            AVLTree::Node {
+                left_child,
+                right_child,
+                height,
+                ..
+            } => {
+                let left_val = (**left_child.borrow()).get_height();
+                let right_val = (**right_child.borrow()).get_height();
+                height.replace(max(left_val, right_val) + 1);
+            }
+        }
+    }
+
+    fn get_height(&self) -> i32 {
+        // Returns the height of a AVLTree node, including empty nodes
+        match self {
+            AVLTree::Empty => {
+                return -1;
+            }
+            AVLTree::Node { height, .. } => {
+                return *height.borrow();
+            }
+        }
+    }
+
+    pub fn leaf_number(&self) -> i32 {
+        // Returns the number of leaves
+        // Note: an empty tree has no leaves
+
+        match self.get_height() {
+            0 => {
+                // leaf node
+                return 1;
+            }
+            -1 => {
+                // empty node
+                return 0;
+            }
+            _ => match self {
+                AVLTree::Empty => {
+                    panic!("leaf_number failed")
+                }
+                AVLTree::Node {
+                    left_child,
+                    right_child,
+                    ..
+                } => {
+                    return (**left_child.borrow()).leaf_number()
+                        + (**right_child.borrow()).leaf_number()
+                }
+            },
+        }
+    }
+
+    pub fn tree_height(&self) -> i32 {
+        // TODO: Dimas
+        match self {
+            AVLTree::Empty => 0,
+            AVLTree::Node { height, .. } => *(height.borrow()),
+        }
+    }
+
+    // pub fn print() {
+    //     // TODO: Dimas
+    // }
+
+    pub fn print_inorder(&self) {
+        // Prints an AVL tree in a inorder traversal
+        match self {
+            AVLTree::Node {
+                data,
+                left_child,
+                right_child,
+                ..
+            } => {
+                (*left_child.borrow()).print_inorder();
+                println!("{}", **data.borrow());
+                (*right_child.borrow()).print_inorder();
+            }
+            AVLTree::Empty => return,
+        }
+    }
+
+    pub fn is_tree_empty(&self) -> bool {
+        match self {
+            AVLTree::Empty => true,
+            AVLTree::Node { .. } => false,
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 fn red_black_insert_random(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("red_black_insert_random");
@@ -778,6 +1135,162 @@ fn red_black_insert_block_sequential(c: &mut Criterion) {
 
 
 
+fn avl_insert_random(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("avl_insert_random");
+    group.sample_size(100);
+    group.warm_up_time(Duration::from_millis(5));
+    group.measurement_time(Duration::from_millis(5));
+
+    for upper in vec![1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000] {
+        let mut insertions:Vec<u32> = (0..upper).collect();
+        let slice: &mut [u32] = &mut insertions;
+        slice.shuffle(&mut thread_rng());
+        insertions = Vec::from(slice);
+
+
+        group.bench_function(
+            ("avl_insert_random_".to_owned() + &upper.to_string()).as_str(),
+            |b| {
+
+                let mut avl = Rc::new(AVLTree::Node {
+                    data: RefCell::new(Rc::new(black_box(&insertions[0]))),
+                    left_child: RefCell::new(Rc::new(AVLTree::Empty)),
+                    right_child: RefCell::new(Rc::new(AVLTree::Empty)),
+                    height: RefCell::new(0),
+                });
+
+
+                for i in 0..insertions.len()-100 {
+                    avl = AVLTree::insert_node(&avl, &&insertions[i]);
+                }
+
+                b.iter(|| {
+                // Code to benchmark goes here
+                    // let y = black_box(x);
+                    for i in insertions.len()-100..insertions.len() {
+                        avl = AVLTree::insert_node(&avl, black_box(&&insertions[i]));
+                    }
+                })
+            },
+        );
+    }
+
+    group.finish();    
+}
+
+fn avl_insert_sequential(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("avl_insert_sequential");
+    group.sample_size(100);
+    group.warm_up_time(Duration::from_millis(5));
+    group.measurement_time(Duration::from_millis(5));
+
+    for upper in vec![1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000] {
+        let mut insertions:Vec<u32> = (0..upper).collect();
+        // let slice: &mut [u32] = &mut insertions;
+        // slice.shuffle(&mut thread_rng());
+        // insertions = Vec::from(slice);
+
+
+        group.bench_function(
+            ("avl_insert_sequential_".to_owned() + &upper.to_string()).as_str(),
+            |b| {
+
+                let mut avl = Rc::new(AVLTree::Node {
+                    data: RefCell::new(Rc::new(black_box(&insertions[0]))),
+                    left_child: RefCell::new(Rc::new(AVLTree::Empty)),
+                    right_child: RefCell::new(Rc::new(AVLTree::Empty)),
+                    height: RefCell::new(0),
+                });
+
+
+                for i in 0..insertions.len()-100 {
+                    avl = AVLTree::insert_node(&avl, &&insertions[i]);
+                }
+
+                b.iter(|| {
+                // Code to benchmark goes here
+                    // let y = black_box(x);
+                    for i in insertions.len()-100..insertions.len() {
+                        avl = AVLTree::insert_node(&avl, black_box(&&insertions[i]));
+                    }
+                })
+            },
+        );
+    }
+
+    group.finish();     
+}
+
+fn avl_insert_block_random(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("red_black_insert_block_random");
+    group.sample_size(100);
+    group.warm_up_time(Duration::from_millis(5));
+    group.measurement_time(Duration::from_millis(5));
+
+    for upper in vec![1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000] {
+        let mut insertions:Vec<u32> = (0..upper).collect();
+        let slice: &mut [u32] = &mut insertions;
+        slice.shuffle(&mut thread_rng());
+        insertions = Vec::from(slice);
+
+
+        group.bench_function(
+            ("red_black_insert_block_random_".to_owned() + &upper.to_string()).as_str(),
+            |b| {
+
+                let mut rbt = RedBlackTree::new(black_box(&insertions[0]));
+                
+                b.iter(|| {
+                // Code to benchmark goes here
+                    // let y = black_box(x);
+                    for i in 0..insertions.len() {
+                        rbt.insert(black_box(&insertions[i]));
+                    }
+                })
+            },
+        );
+    }
+
+    group.finish();    
+}
+
+fn avl_insert_block_sequential(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("red_black_insert_block_sequential");
+    group.sample_size(100);
+    group.warm_up_time(Duration::from_millis(5));
+    group.measurement_time(Duration::from_millis(5));
+
+    for upper in vec![1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000] {
+        let mut insertions:Vec<u32> = (0..upper).collect();
+        // let slice: &mut [u32] = &mut insertions;
+        // slice.shuffle(&mut thread_rng());
+        // insertions = Vec::from(slice);
+
+
+        group.bench_function(
+            ("red_black_insert_random_".to_owned() + &upper.to_string()).as_str(),
+            |b| {
+
+                let mut rbt = RedBlackTree::new(black_box(&insertions[0]));
+                
+
+                b.iter(|| {
+                // Code to benchmark goes here
+                    // let y = black_box(x);
+                    for i in 0..insertions.len() {
+                        rbt.insert(black_box(&insertions[i]));
+                    }
+                })
+            },
+        );
+    }
+
+    group.finish();    
+}
 
 
 
@@ -786,5 +1299,9 @@ fn red_black_insert_block_sequential(c: &mut Criterion) {
 
 
 
-criterion_group!(benches, red_black_insert_random, red_black_insert_sequential, red_black_insert_block_random, red_black_insert_block_sequential);
+
+
+// criterion_group!(benches, red_black_insert_random, red_black_insert_sequential, red_black_insert_block_random, red_black_insert_block_sequential);
+// criterion_group!(benches, red_black_insert_random, avl_insert_random);
+criterion_group!(benches, red_black_insert_sequential, avl_insert_sequential);
 criterion_main!(benches);
