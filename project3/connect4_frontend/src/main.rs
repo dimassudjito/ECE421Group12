a68e61ac7f4ed59554cc92a999716f1dadcb4340
Zony Yu <zony249@gmail.com>
2023-03-27 21:37:32 -0600

    board insert and debug print

--- project3/connect4_frontend/src/main.rs
+++ project3/connect4_frontend/src/main.rs
@@ -1,5 +1,9 @@
 use yew::prelude::*;
 
+
+mod board;
+use board::*;
+
 #[function_component(App)]
 fn app() -> Html {
     html! {
@@ -8,5 +12,31 @@ fn app() -> Html {
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    // yew::Renderer::<App>::new().render();
+
+    //// testing board /////
+
+    let mut board = Board::<i32>::new(6, 8);
+
+    board.insert(&1, None, Some(3));
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(3));
+
+
+    board.insert(&0, None, Some(4));
+    board.insert(&0, None, Some(3));
+    board.insert(&0, None, Some(4));
+
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(3));
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(4));
+    board.insert(&1, None, Some(4));
+    
+    println!("{:?}", board.container);
+    board.debug_print();
+    //// end testing board ////
 }
