f29ad0d0190218c5705fb4a7414ee4ecbde85dd1
jyw2 <jyuenw@gmail.com>
2023-03-30 17:07:09 -0600

    Add non-static tables with dummy data

--- project3/connect4_frontend/src/components/routes/history.rs
+++ project3/connect4_frontend/src/components/routes/history.rs
@@ -1,7 +1,30 @@
+use crate::components::table::Table;
 use yew::prelude::*;
 #[function_component]
 pub fn HistoryPage() -> Html {
-    html! {
+    let headers = vec![
+        "Game-ID",
+        "Game Type",
+        "Player1",
+        "Player2",
+        "Winner",
+        "When Played",
+    ];
+    let data = vec![
+        vec!["0", "Otto", "Peter", "Polly", "Polly", "10-04-5002"],
+        vec!["1", "Connect4", "Johanne", "Jax", "Jax", "11-01-2202"],
+    ];
+    let fallback = html! {<h3>{"Loading..."}</h3>};
 
-    <h1>{"history"}</h1>}
+    html! {
+        <div class="w3-container" id="services" style="margin-top:75px">
+            <h1 ><b>{"Game History"}</b></h1>
+
+                <div id="game-stream">
+                    <Suspense {fallback}>
+                        <Table headers = {headers} data ={data}></Table>
+                    </Suspense>
+                </div>
+        </div>
+    }
 }
