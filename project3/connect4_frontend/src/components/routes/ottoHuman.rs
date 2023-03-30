7ccec1d592d11dcf2d3955cc207b2bc5820da544
Dimas Sudjito <almuhtad@ualberta.ca>
2023-03-30 16:38:20 -0600

    basic functionality of form binding

--- project3/connect4_frontend/src/components/routes/ottoHuman.rs
+++ project3/connect4_frontend/src/components/routes/ottoHuman.rs
@@ -1,19 +1,35 @@
 use yew::prelude::*;
 
+use crate::components::text_input::TextInput;
+
 #[function_component(OttoHumanPage)]
 pub fn otto_human_page() -> Html {
+    let player1_name = use_state(|| "UNKNOWN".to_owned());
+    let cloned_player1_name = player1_name.clone();
+    let player1_name_changed = Callback::from(move |username: String| {
+        cloned_player1_name.set(username);
+    });
+
+    let player2_name = use_state(|| "UNKNOWN".to_owned());
+    let cloned_player2_name = player2_name.clone();
+    let player2_name_changed = Callback::from(move |username: String| {
+        cloned_player2_name.set(username);
+    });
+
     html! {
         <div>
             <h5 class="w3-xxxlarge w3-text-red"><b>{"Enter Player Names"}</b></h5>
             <hr style="width:50px;border:5px solid red" class="w3-round"/>
             <form>
-                <input id="textbox1" type="text" placeholder="Player 1's Name" ng-model="newGame.Player1Name"/>
-                <input id="textbox2" type="text" placeholder="Player 2's Name" ng-model="newGame.Player2Name"/>
+                // <input id="textbox1" type="text" placeholder="Player 1's Name" ng-model="newGame.Player1Name"/>
+                // <input id="textbox2" type="text" placeholder="Player 2's Name" ng-model="newGame.Player2Name"/>
                 <input id="startbutton" class="button" type="submit" value="Start Game"/>
+                <TextInput name="textbox1" handle_onchange={player1_name_changed} />
+                <TextInput name="textbox2" handle_onchange={player2_name_changed} />
             </form>
             <br/>
-            <h4>{"New Game: <player1.name> Vs <player2.name>"}</h4>
-            <small>{"(Winning Combination: <player1.name> - TOOT and <player2.name> - OTTO)"}</small>
+            <h4>{format!("New Game: {} Vs {}", &*player1_name, &*player2_name)}</h4>
+            <small>{format!("(Winning Combination: {} - TOOT and {} - OTTO)", &*player1_name, &*player2_name)}</small>
             <br/>
             <form>
                 <h4>{"Select a Disc Type    :"}</h4>
