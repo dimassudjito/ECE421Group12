df9ef598c1a3372f502e2de4f2584058525d6d25
jyw2 <jyuenw@gmail.com>
2023-04-01 16:32:49 -0600

    Add reactive collapsing nav

--- project3/connect4_frontend/src/main.rs
+++ project3/connect4_frontend/src/main.rs
@@ -1,6 +1,7 @@
 mod components;
 // mod routes;
 use crate::components::routes::*;
+use stylist::yew::use_media_query;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
@@ -45,47 +46,80 @@ fn switch(routes: Route) -> Html {
     }
 }
 
-
 mod board;
 use board::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    // let navigator = use_navigator().unwrap();
-    // let navigate = Callback::from(move |_: &str| navigator.push(&Route::Home));
+    let isSmall = use_media_query("(max-width: 1100px)");
+    let isNavVis = use_state(|| !isSmall);
+
+    let isNavVisClone = isNavVis.clone();
+    use_effect_with_deps(move |_| isNavVisClone.set(false), isSmall);
+
+    let showNav = {
+        let isNavVis = isNavVis.clone();
+        Callback::from(move |_| isNavVis.set(true))
+    };
+
+    let hideNav = {
+        let isNavVis = isNavVis.clone();
+        Callback::from(move |_| isNavVis.set(false))
+    };
+
     html! {
         <BrowserRouter>
-            <div style="display:flex;flex-direction:row">
+                <div style="display:flex;flex-direction:row;position:absolute;top:0;width:100%">
 
+                    if isSmall && !*isNavVis {
 
-                <nav class="side-nav" id="mySidenav">
-                    <div style="display:flex;flex-direction:column;width:320px">
-                        <br/>
-                        <br/>
-                        <div class="w3-container">
-                            <h3 class="w3-padding title"><b>{"Play"}<br/>{"Connect4 / TOOT-OTTO"}</b></h3>
+                        <div onclick={showNav} class="mobile-hamburger">
+                            <div class="mobile-hamburger-slice"></div>
+                            <div class="mobile-hamburger-slice"></div>
+                            <div class="mobile-hamburger-slice"></div>
+                            <div class="mobile-hamburger-slice"></div>
                         </div>
-                        <br/>
-                        <Link<Route> to={Route::Connect4Help} classes="w3-padding w3-hover-white">{"How to Play Connect4"}</Link<Route>>
-                        <Link<Route> to={Route::Connect4Computer} classes="w3-padding w3-hover-white">{"Play Connect4 With Computer"}</Link<Route>>
-                        <Link<Route> to={Route::Connect4Local} classes="w3-padding w3-hover-white">{"Play Connect4 with Another Human"}</Link<Route>>
-                        <br/>
-                        <br/>
-                        <Link<Route> to={Route::OttoHelp} classes="w3-padding w3-hover-white">{"How to Play TOOT-OTTO"}</Link<Route>>
-                        <Link<Route> to={Route::OttoComputer} classes="w3-padding w3-hover-white">{"Play Toot-Otto With Computer"}</Link<Route>>
-                        <Link<Route> to={Route::OttoLocal} classes="w3-padding w3-hover-white">{"Play Toot-Otto With Another Human"}</Link<Route>>
-                        <br/>
-                        <br/>
-                        <Link<Route> to={Route::History} classes="w3-padding w3-hover-white">{"View Game History"}</Link<Route>>
-                        <Link<Route> to={Route::Score} classes="w3-padding w3-hover-white">{"Score Board"}</Link<Route>>
+                    }else{
+                        <nav class="side-nav" id="mySidenav">
+                            <div style="display:flex;flex-direction:column;width:320px">
+                                if isSmall {
+                                    <a onclick={hideNav.clone()} class="w3-hover-white">{"close navigation"}</a>
+                                }else{
+                                    <br/>
+                                }
+                                <div class="w3-container">
+                                    <h3 class="w3-padding title"><b>{"Play"}<br/>{"Connect4 / TOOT-OTTO"}</b></h3>
+                                </div >
+                                <br/>
+                                <div onclick={hideNav.clone()} class="nav-section">
+                                    <Link<Route>  to={Route::Connect4Help} classes="w3-padding w3-hover-white">{"How to Play Connect4"}</Link<Route>>
+                                    <Link<Route> to={Route::Connect4Computer} classes="w3-padding w3-hover-white">{"Play Connect4 With Computer"}</Link<Route>>
+                                    <Link<Route> to={Route::Connect4Local} classes="w3-padding w3-hover-white">{"Play Connect4 with Another Human"}</Link<Route>>
+                                </div>
+                                <br/>
+                                <br/>
+                                <div onclick={hideNav.clone()} class="nav-section">
+                                    <Link<Route> to={Route::OttoHelp} classes="w3-padding w3-hover-white">{"How to Play TOOT-OTTO"}</Link<Route>>
+                                    <Link<Route> to={Route::OttoComputer} classes="w3-padding w3-hover-white">{"Play Toot-Otto With Computer"}</Link<Route>>
+                                    <Link<Route> to={Route::OttoLocal} classes="w3-padding w3-hover-white">{"Play Toot-Otto With Another Human"}</Link<Route>>
+                                </div>
+                                <br/>
+                                <br/>
+                                <div onclick={hideNav.clone()} class="nav-section">
+                                    <Link<Route> to={Route::History} classes="w3-padding w3-hover-white">{"View Game History"}</Link<Route>>
+                                    <Link<Route> to={Route::Score} classes="w3-padding w3-hover-white">{"Score Board"}</Link<Route>>
+                                </div>
+                            </div>
+                        </nav>
+                    }
+                    <div style="padding:5%;width:100%">
+
+                            <Switch<Route> render={switch} />
+
                     </div>
-                </nav>
-                <div style="padding:5%;width:100%">
-
-                        <Switch<Route> render={switch} />
-
                 </div>
-            </div>
+
+
         </BrowserRouter>
     }
 }
@@ -101,7 +135,6 @@ fn main() {
     board.insert(&1, None, Some(4));
     board.insert(&1, None, Some(3));
 
-
     board.insert(&0, None, Some(4));
     board.insert(&0, None, Some(3));
     board.insert(&0, None, Some(4));
@@ -114,7 +147,7 @@ fn main() {
     board.insert(&1, None, Some(4));
     board.insert(&1, None, Some(4));
     board.insert(&1, None, Some(4));
-    
+
     println!("{:?}", board.container);
     board.debug_print();
     //// end testing board ////
