4954ffbdd21ca93ef19c555eb254c3e0b506f5b1
Jie-F <78284005+Jie-F@users.noreply.github.com>
2023-04-02 15:47:42 -0600

    Added basic REST API for MongoDB with a test schema

--- project3/connect4_backend/src/main.rs
+++ project3/connect4_backend/src/main.rs
@@ -1,9 +1,17 @@
 #![feature(decl_macro)]
 #![feature(proc_macro_hygiene)]
 
+//add the modules
+mod api; 
+mod models;
+mod repository;
+
 #[macro_use]
 extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
+use mongodb::{Client, options::ClientOptions};
 
+/*
 #[get("/hello/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
@@ -16,4 +24,21 @@ fn hi(name: String) -> String {
 
 fn main() {
     rocket::ignite().mount("/", routes![hello, hi]).launch();
+}
+*/
+
+//add imports below
+use api::user_api::{create_user, get_user, update_user, delete_user, get_all_users};
+use repository::mongodb_repo::MongoRepo;
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![create_user])
+        .mount("/", routes![get_user])
+        .mount("/", routes![update_user])
+        .mount("/", routes![delete_user])
+        .mount("/", routes![get_all_users])
 }
